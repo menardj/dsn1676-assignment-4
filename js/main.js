d733400a93db9f12c970b2f496bc8259a9c6769b
menardj <JesseMenard@Jesses-MacBook.local>
2015-02-09 18:44:54 -0500

    Javascript
    Began inputting variables on main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var blueberries = {
+	species: 'Vaccinium corymbosum',
+	growsOnShrub: true,
+	energy: '240 kJ',
+	carbohydrates: '14.49 g',
+	protein: '0.74 g',
+};
+
+var grapes = {
+	species: 'Vitis vinifera',
+	growsOnShrub: false,
+	energy: '288 kJ',
+	carbohydrates: '18.1 g',
+	protein: '0.72 g'
+};
+
+var redCurrant = {
+	species: 'Ribes rubrum',
+	growsOnShrub: true,
+	energy: '234 kJ',
+	carbohydrates: '13.8 g',
+	protein: '1.4 g'
+};
